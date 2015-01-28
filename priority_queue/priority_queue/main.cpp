e8709bab5fc2ba9e01be270a1028ed5891c1db00
Yulia Khudiakova <yulia.khudiakova@gmail.com>
2015-01-28 14:26:15 +0300

    heap implementation

---
+++ priority_queue/priority_queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  priority_queue
+//
+//  Created by Yulia Khudiakova on 28.01.15.
+//  Copyright (c) 2015 Yulia Khudiakova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
